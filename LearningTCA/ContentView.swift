83e7bd69d62b120ea3a5a27454795a2555ac605c
davdroman <d@vidroman.dev>
2021-07-22 14:36:36 +0100

    Add base project

---
+++ LearningTCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningTCA
+//
+//  Created by Rebecca Schwartz on 22/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningTCA/LearningTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningTCAApp.swift
+//  LearningTCA
+//
+//  Created by Rebecca Schwartz on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
