fb2adce81a8da1ff5427a9a2572e7ace4d582304
David Roman <d@vidroman.dev>
2022-03-29 18:49:07 +0100

    Refine `MultilineTextField` self-sizing

--- MultilineTextField/Sources/MultilineTextField/MultilineTextField.swift
+++ MultilineTextField/Sources/MultilineTextField/MultilineTextField.swift
@@ -7,6 +7,11 @@ public struct MultilineTextField: View {
     private var text: String
     private var minHeight: CGFloat
 
+    @Weak
+    private var view: UITextView?
+    @State
+    private var delegate: NSTextStorageDelegate?
+
     public init(_ placeholder: String, text: Binding<String>, minHeight: CGFloat? = nil) {
         self.placeholder = placeholder
         self._text = text
@@ -15,68 +20,108 @@ public struct MultilineTextField: View {
 
     public var body: some View {
         ZStack(alignment: .topLeading) {
-            TextField("", text: .constant(""))
-                .hidden()
-                .background(
-                    GeometryReader {
-                        Color.clear.preference(
-                            key: TextFieldMinHeightKey.self,
-                            value: $0.frame(in: .local).size.height
-                        )
-                    }
-                )
-
-            Text(text)
-                .fixedSize(horizontal: false, vertical: true)
-                .hidden()
-                .background(
-                    GeometryReader {
-                        Color.clear.preference(
-                            key: TextHeightKey.self,
-                            value: $0.frame(in: .local).size.height
-                        )
-                    }
-                )
-
             if text.isEmpty {
                 Text(placeholder)
                     .foregroundColor(Color(.tertiaryLabel))
             }
 
             TextEditor(text: $text)
-                .frame(height: max(minHeight, textFieldMinHeight, textHeight).rounded(.up))
+                .frame(height: max(minHeight, textHeight).rounded(.up))
                 .introspectTextView {
+                    // collect view instance
+                    view = $0
+
+                    // observe text changes via delegate
+                    delegate = NSTextStorageDelegateInstance(onEditing: refreshTextHeightOnNextRunLoopPass)
+                    $0.textStorage.delegate = delegate
+
+                    // set misc properties
                     $0.isScrollEnabled = false
                     $0.backgroundColor = .clear
                     $0.textContainerInset = .zero
                     $0.textContainer.lineFragmentPadding = .zero
                 }
-        }
-        .onPreferenceChange(TextFieldMinHeightKey.self) {
-            textFieldMinHeight = $0
-        }
-        .onPreferenceChange(TextHeightKey.self) {
-            textHeight = $0
+                .onAppear(perform: refreshTextHeightOnNextRunLoopPass)
+                .onSizeChange(perform: refreshTextHeightOnNextRunLoopPass)
         }
     }
 
-    @State private var textFieldMinHeight: CGFloat = 0
     @State private var textHeight: CGFloat = 0
-}
 
-private struct TextFieldMinHeightKey: PreferenceKey {
-    static var defaultValue: CGFloat { 0 }
-    static func reduce(value: inout Value, nextValue: () -> Value) {
-        value = value + nextValue()
+    private func refreshTextHeightOnNextRunLoopPass() {
+        DispatchQueue.main.async(execute: refreshTextHeight)
+    }
+
+    private func refreshTextHeight() {
+        guard let view = view else {
+            return
+        }
+
+        let currentTextHeight = self.textHeight
+        let proposedTextHeight = view.idealTextHeight()
+
+        guard proposedTextHeight != currentTextHeight else {
+            return
+        }
+
+        textHeight = proposedTextHeight
     }
 }
 
-private struct TextHeightKey: PreferenceKey {
-    static var defaultValue: CGFloat { 0 }
-    static func reduce(value: inout Value, nextValue: () -> Value) {
-        value = value + nextValue()
+extension UITextView {
+    fileprivate func idealTextHeight() -> CGFloat {
+        let newSize = self.sizeThatFits(CGSize(width: self.frame.size.width, height: .greatestFiniteMagnitude))
+        let newHeight = newSize.height
+        return newHeight
     }
 }
+
+@propertyWrapper
+private class Weak<T: AnyObject> {
+    var wrappedValue: T? {
+        get { weakValue }
+        set { weakValue = newValue }
+    }
+
+    weak var weakValue: T?
+
+    init(wrappedValue: T?) {
+        self.weakValue = wrappedValue
+    }
+}
+
+private final class NSTextStorageDelegateInstance: NSObject, NSTextStorageDelegate {
+    typealias OnEditing = () -> Void
+
+    let onEditing: OnEditing
+
+    init(onEditing: @escaping OnEditing) {
+        self.onEditing = onEditing
+        super.init()
+    }
+
+    func textStorage(_ textStorage: NSTextStorage, didProcessEditing editedMask: NSTextStorage.EditActions, range editedRange: NSRange, changeInLength delta: Int) {
+        onEditing()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func onSizeChange(perform action: @escaping () -> Void) -> some View {
+        self.background(
+            GeometryReader { proxy in
+                Color.clear.preference(key: SizePreferenceKey.self, value: proxy.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self) { _ in action() }
+    }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+}
+
 
 struct MultilineTextField_Previews: PreviewProvider {
     static var previews: some View {
