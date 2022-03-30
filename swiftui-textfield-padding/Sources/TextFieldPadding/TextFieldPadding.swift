36ce1114baed440d7da7e79e330a4ab0976ffca7
David Roman <d@vidroman.dev>
2022-03-30 17:10:05 +0100

    [TFP] Reimplement using Obj-C runtime voodoo

--- swiftui-textfield-padding/Sources/TextFieldPadding/TextFieldPadding.swift
+++ swiftui-textfield-padding/Sources/TextFieldPadding/TextFieldPadding.swift
@@ -4,7 +4,14 @@ import SwiftUI
 extension View {
     @ViewBuilder
     public func textFieldPadding(_ insets: EdgeInsets) -> some View {
-        self.modifier(TextFieldPaddingModifier(insets: insets))
+        Group {
+            if self.isTextFieldPaddingModifierApplied {
+                self
+            } else {
+                self.modifier(TextFieldPaddingModifier())
+            }
+        }
+        .environment(\.textFieldPadding, insets)
     }
 
     @ViewBuilder
@@ -20,91 +27,48 @@ extension View {
     }
 }
 
-private struct TextFieldPaddingModifier: ViewModifier {
-    @Weak
-    private var view: UITextField?
+private extension View {
+    // TODO: strengthen this logic somehow... maybe through reflection?
+    var isTextFieldPaddingModifierApplied: Bool {
+        let modifierTypeName = String(describing: TextFieldPaddingModifier.self)
+        let currentTypeName = String(describing: type(of: self))
+        return currentTypeName.contains(modifierTypeName)
+    }
+}
 
-    let insets: EdgeInsets
+private struct TextFieldPaddingModifier: ViewModifier {
+    @Environment(\.textFieldPadding)
+    private var padding
 
     func body(content: Content) -> some View {
         content
-            .introspectTextField { view = $0 }
-            .padding(insets)
-            .background(LocatedTap(in: view) { point in
-                guard let view = view else { return }
-                let newPosition = view.closestPosition(to: point) ?? view.endOfDocument
-                view.becomeFirstResponder()
-                view.selectedTextRange = view.textRange(from: newPosition, to: newPosition)
-            })
+            .introspectTextField {
+                $0.textRectInsets = UIEdgeInsets(
+                    top: padding.top,
+                    left: padding.leading,
+                    bottom: padding.bottom,
+                    right: padding.trailing
+                )
+            }
     }
 }
 
-@propertyWrapper
-private class Weak<T: AnyObject> {
-    var wrappedValue: T? {
-        get { weakValue }
-        set { weakValue = newValue }
+private extension EnvironmentValues {
+    var textFieldPadding: EdgeInsets {
+        get { self[TextFieldPaddingKey.self] }
+        set { self[TextFieldPaddingKey.self] = newValue }
     }
 
-    weak var weakValue: T?
-
-    init(wrappedValue: T?) {
-        self.weakValue = wrappedValue
+    struct TextFieldPaddingKey: EnvironmentKey {
+        static let defaultValue: EdgeInsets = .init()
     }
-}
-
-private struct LocatedTap: UIViewRepresentable {
-    var localView: () -> UIView?
-    var handler: (CGPoint) -> Void
-
-    init(
-        in localView: @escaping @autoclosure () -> UIView?,
-        handler: @escaping (CGPoint) -> Void
-    ) {
-        self.localView = localView
-        self.handler = handler
-    }
-
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: .zero)
-        view.addGestureRecognizer(
-            UITapGestureRecognizer(
-                target: context.coordinator,
-                action: #selector(Coordinator.tapped)
-            )
-        )
-        return view
-    }
-
-    final class Coordinator: NSObject {
-        var localView: () -> UIView?
-        var handler: (CGPoint) -> Void
-
-        init(
-            localView: @escaping () -> UIView?,
-            handler: @escaping (CGPoint) -> Void
-        ) {
-            self.localView = localView
-            self.handler = handler
-        }
-
-        @objc func tapped(gesture: UITapGestureRecognizer) {
-            let point = gesture.location(in: localView() ?? gesture.view)
-            self.handler(point)
-        }
-    }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(localView: localView, handler: handler)
-    }
-
-    func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
 struct TextFieldPadding_Previews: PreviewProvider {
     static var previews: some View {
         TextField("Placeholder", text: .constant("Lorem ipsum dolor sit amet"))
-            .textFieldPadding(32)
+            .background(Color.blue)
+            .textFieldPadding(30)
             .background(Color.red)
             .previewLayout(.sizeThatFits)
     }
