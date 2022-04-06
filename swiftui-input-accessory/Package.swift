524d197ba08876f7b8384d87bfe408c75edb8b93
David Roman <d@vidroman.dev>
2022-04-06 15:49:54 +0100

    [IA] Move dependencies to bottom of manifest

--- swiftui-input-accessory/Package.swift
+++ swiftui-input-accessory/Package.swift
@@ -10,10 +10,11 @@ let package = Package(
     products: [
         .library(name: "SwiftUIInputAccessory", targets: ["SwiftUIInputAccessory"]),
     ],
-    dependencies: [
-        .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.3"),
-    ],
     targets: [
         .target(name: "SwiftUIInputAccessory", dependencies: [.product(name: "Introspect", package: "Introspect")]),
     ]
 )
+
+package.dependencies = [
+    .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.3"),
+]
