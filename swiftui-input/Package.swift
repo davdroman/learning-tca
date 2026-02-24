c1c4e1b6269cf7bc542898e39bceb9e04272e75c
David Roman <2538074+davdroman@users.noreply.github.com>
2026-02-24 10:29:37 +0000

    gardening

--- swiftui-input/Package.swift
+++ swiftui-input/Package.swift
@@ -3,20 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftui-input",
-    platforms: [
-        .iOS(.v15)
-    ],
-    products: [
-        .library(name: "SwiftUIInput", targets: ["SwiftUIInput"]),
-    ],
-    targets: [
-        .target(name: "SwiftUIInput", dependencies: [
-            .product(name: "SwiftUIIntrospect", package: "swiftui-introspect")
-        ]),
-    ]
+	name: "swiftui-input",
+	platforms: [
+		.iOS(.v15)
+	],
+	products: [
+		.library(name: "SwiftUIInput", targets: ["SwiftUIInput"]),
+	],
+	targets: [
+		.target(name: "SwiftUIInput", dependencies: [
+			.product(name: "SwiftUIIntrospect", package: "swiftui-introspect")
+		]),
+	]
 )
 
 package.dependencies = [
-    .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
+	.package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0"),
 ]
